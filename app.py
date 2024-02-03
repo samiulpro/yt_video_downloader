eaac95cb97047b1cdb4d61d5aa75820c45e5318a
Samiul Islam sami <samiulislamsami@Samiuls-Air.Dlink>
2024-02-04 00:36:01 +0600

    Proper MP3 conversion added

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from pytube import YouTube
+from moviepy.editor import *
+import os
+os.environ["IMAGEIO_FFMPEG_EXE"] = "/usr/bin/ffmpeg"
+import random
+import shutil
 
 def download_mp3(link, file_path):
 
-    save_file_loc = file_path
+    workdir = str(random.random())
+    os.mkdir(workdir)
+    save_file_loc = file_path + '/' + workdir
     youtube_obj = YouTube(link)
-    resolution = youtube_obj.streams.filter(only_audio=True).first()
+    resolution = youtube_obj.streams.get_highest_resolution()
     resolution.download(save_file_loc)
-
+    name = os.listdir(save_file_loc)
+    vidname = name[0]
+    video = VideoFileClip(f"{save_file_loc}/{vidname}")
+    video.audio.write_audiofile(f'{vidname}.mp3')
+    shutil.rmtree(save_file_loc)
 
 def download_mp4(link, file_path):
 
